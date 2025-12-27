ecbd1e0a5c8a9ea28f31e4348dc702cf88d4546e
Chamodi Sandeepani <sandeepanichamodi999@gmail.com>
2025-12-27 08:16:31 +0530

    stock,stock_register

--- Predictive-Income-and-Repair-Time-Analytics-and-Forecasting-System-for-Multi9-Computer-Systems/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "servidedb");
-
-if (!$conn) {
-    die("Database connection failed");
-}
-?>
--- Predictive-Income-and-Repair-Time-Analytics-and-Forecasting-System-for-Multi9-Computer-Systems/index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'includes/navbar.php'; ?>
-
-<link rel="stylesheet" href="assets/css/style.css">
-
-<div class="container">
-    <h2>Welcome to Multi9 Computer Systems</h2>
-</div>
