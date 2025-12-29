b76eda654b41d8857736a085a3e12006bdc53586
Mithila Medhavi <mithilamedhavi2@gmail.com>
2025-12-29 14:36:28 +0530

    update

--- db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// කිසිදු හිස්තැනක් (Space) නොමැතිව පහත පරිදි ලියන්න
-$conn = new mysqli("localhost", "root", "", "servidedb");
-
-if ($conn->connect_error) { 
-    die("සම්බන්ධතාවය බිඳ වැටුණි: " . $conn->connect_error); 
-}
-?>
