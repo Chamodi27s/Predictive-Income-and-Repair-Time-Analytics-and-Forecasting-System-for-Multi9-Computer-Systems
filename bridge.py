4e7450bc570f1a2f7eb0b962a9e7ea7c2cfd2153
anu123538 <anuththaraimanshi842@gmail.com>
2026-08-08 19:47:25 +0000

    add

--- bridge.py
+++ bridge.py
@@ -3,15 +3,15 @@ import joblib
 import pandas as pd
 import os
 
-# ෆයිල් එක තියෙන තැන හරියටම ගන්න
+
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-# Model සහ Encoders load කිරීම
+
 model = joblib.load(os.path.join(base_path, 'repair_model.pkl'))
 encoders = joblib.load(os.path.join(base_path, 'label_encoders.pkl'))
 
-# PHP වලින් එවනු ලබන දත්ත ලබා ගැනීම
-# sys.argv[1] සිට sys.argv[5] දක්වා පිළිවෙලින් දත්ත ලැබෙයි
+
+
 try:
     input_data = {
         'Device_Type': sys.argv[1],
@@ -23,19 +23,19 @@ try:
 
     input_df = pd.DataFrame([input_data])
 
-    # අකුරු ඉලක්කම් වලට හරවා ගැනීම (Encoding)
+    
     for col, le in encoders.items():
         if col in input_df.columns:
-            # අලුත් දත්තයක් ආවොත් (Unseen labels) එය handle කිරීම
+            
             val = str(input_df[col][0])
             if val in le.classes_:
                 input_df[col] = le.transform([val])
             else:
                 input_df[col] = 0 
 
-    # අනාවැකිය (Prediction)
+
     prediction = model.predict(input_df)
-    print(round(prediction[0], 2)) # මෙහි ප්‍රතිඵලය පමණක් PHP එකට ලැබේ
+    print(round(prediction[0], 2)) 
 
 except Exception as e:
     print(f"Error: {str(e)}")
