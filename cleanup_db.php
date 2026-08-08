fd207c2c4d985fcfa720acb42ac3d32f63392cd0
Mithila Medhavi <mithilamedhavi2@gmail.com>
2026-08-08 21:45:42 +0530

    update

--- cleanup_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db_config.php';
-mysqli_query($conn, "DELETE FROM job WHERE job_no = 'ORD-5001'");
-mysqli_query($conn, "DELETE FROM job WHERE job_no = 'ORD-5000'"); // In case 5000 is also corrupted
-echo "Deleted corrupted jobs.";
-?>
